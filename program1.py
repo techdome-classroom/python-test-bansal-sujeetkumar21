ab7598e2008e4ea4d0bc8062256652fba05d84d5
SUJEET KUMAR <108637141+sujeetkumar21@users.noreply.github.com>
2024-02-23 12:57:16 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,10 +1,19 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+        
+        opcl = dict(('()', '[]', '{}'))
+        
+        stack = []
+      
+        for idx in s:
+           
+            if idx in '([{':
+                stack.append(idx)
+           
+            elif len(stack) == 0 or idx != opcl[stack.pop()]:
+                return False
+        
+        return len(stack) == 0
     
 
 
